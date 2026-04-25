27dd1e04b06c5f1b23798a6339d89a9cabbf9e67
ruzen42 <malledeferlive@gmail.com>
2026-04-25 18:36:45 +0000

    add 512

--- src/Hash.hs
+++ src/Hash.hs
@@ -12,13 +12,14 @@ import GHC.Generics (Generic)
 import Crypto.Hash
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
+import qualified Data.ByteArray as BA
 import qualified Data.ByteArray.Encoding as BAE
 
 data HashType 
-  = SHA2_256 
-  | SHA2_512 
-  | SHA3_256 
-  | SHA3_512 
+  = SHAT256 
+  | SHAT512 
+  | SHAH256 
+  | SHAH512 
   deriving Generic
 
 
@@ -35,15 +36,20 @@ postHashR = do
     body <- requireCheckJsonBody :: Handler HashRequest 
     
     let hashMethod = case hash_method body of 
-                    SHA2_256 -> hashT256 
-                    SHA2_512 -> hashT256 
-                    _        -> hashT256 
+                    SHAT256 -> hashT256 
+                    SHAT512 -> hashT512 
+                    SHAH512 -> hashT256 
+                    SHAH256 -> hashT512 
+                    _       -> hashT256 
 
     return $ toHex $ hashMethod (source body)
 
 hashT256 :: Text -> Digest SHA256
 hashT256 t = hash (TE.encodeUtf8 t)
 
-toHex :: Digest SHA256 -> T.Text
+hashT512 :: Text -> Digest SHA256
+hashT512 t = hash (TE.encodeUtf8 t)
+
+toHex :: BA.ByteArrayAccess ba => ba -> T.Text
 toHex = TE.decodeUtf8 . BAE.convertToBase BAE.Base16
 
